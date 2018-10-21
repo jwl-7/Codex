c9c9c4d1fa7bdcf6fec511afa6ea0a16912ac157
jwl-7 <lucidrainbows@gmail.com>
2018-10-21 05:04:30 -0400

    converted camelCase variable names to snake_case

--- commands.py
+++ commands.py
@@ -5,23 +5,25 @@ class Commands:
 
     # list of bot commands
     def help(self):
-        commandList = ("```!8ball - Magic 8-Ball\n" 
+        command_list = ("```!8ball - Magic 8-Ball\n" 
                           "!coin - Heads or Tails\n"
                           "!slots - Slot Machine\n" 
                           "!sponge - Spongebob Meme Text\n```")
 
-        return commandList
+        return command_list
 
     # magic 8-ball
     def magic_8ball(self):
-        responses = ['It is certain.', 'It is decidedly so.', 'Without a doubt.',
-                   'Yes - definitely.', 'You may rely on it.', 'As I see it, yes.',
-                   'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to yes.',
-                   'Reply hazy, try again.', 'Ask again later.',
-                   'Better not tell you now.', 'Cannot predict now.',
-                   'Concentrate and ask again.', 'Do not count on it.',
-                   'My reply is no.', 'My sources say no.',
-                   'Outlook not so good.', 'Very doubtful.']
+        responses = ['It is certain.', 'It is decidedly so.',
+                     'Without a doubt.', 'Yes - definitely.', 
+                     'You may rely on it.', 'As I see it, yes.',
+                     'Most likely.', 'Outlook good.', 
+                     'Yes.', 'Signs point to yes.', 
+                     'Reply hazy, try again.', 'Ask again later.', 
+                     'Better not tell you now.', 'Cannot predict now.', 
+                     'Concentrate and ask again.', 'Do not count on it.', 
+                     'My reply is no.', 'My sources say no.', 
+                     'Outlook not so good.', 'Very doubtful.']
         
         return random.choice(responses)
 
@@ -39,25 +41,25 @@ class Commands:
         slot4 = slots[random.randint(0, 5)]
 
         # display slot machine 
-        slotRoll = '|\t:{}:\t|\t:{}:\t|\t:{}:\t|\t:{}:\t|    '.format(slot1, slot2, slot3, slot4)
+        slot_spin = '|\t:{}:\t|\t:{}:\t|\t:{}:\t|\t:{}:\t|    '.format(slot1, slot2, slot3, slot4)
         jackpot = '\n< $$$ !!! JACKPOT !!! $$$ >'
 
         if (slot1 == slot2 and slot3 == slot4 or slot1 == slot3 and slot2 == slot4 
                                               or slot1 == slot4 and slot2 == slot3):
-            return slotRoll + jackpot
+            return slot_spin + jackpot
         else:
-            return slotRoll
+            return slot_spin
 
     # converts message to spongebob mocking meme text
     def sponge(self, item_text):
-        sText = item_text
-        sText = sText.replace('!sponge ', '')
-        spongeText = ''
+        text = item_text
+        text = text.replace('!sponge ', '')
+        sponge_text = ''
 
-        for i, s in enumerate(sText):
+        for i, s in enumerate(text):
             if i % 2 == 0:
-                spongeText += s.lower()
+                sponge_text += s.lower()
             else:
-                spongeText += s.upper()
+                sponge_text += s.upper()
 
-        return spongeText
+        return sponge_text
