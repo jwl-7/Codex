c00c566a405d5ea60ce59c6a68e5b6b6d0719d18
jwl-7 <lucidrainbows@gmail.com>
2018-10-21 17:55:52 -0400

    reformatted 8-ball responses list and added !wisdom to command_list

--- commands.py
+++ commands.py
@@ -5,25 +5,36 @@ class Commands:
 
     # list of bot commands
     def help(self):
-        command_list = ("```!8ball - Magic 8-Ball\n" 
-                          "!coin - Heads or Tails\n"
-                          "!slots - Slot Machine\n" 
-                          "!sponge - Spongebob Meme Text\n```")
+        command_list = ("```!8ball - magic 8-ball\n" 
+                           "!coin - heads or tails\n"
+                           "!slots - slot machine\n" 
+                           "!sponge - spongebob meme text\n"
+                           "!wisdom - deepak chopra quote```")
 
         return command_list
 
     # magic 8-ball
     def magic_8ball(self):
-        responses = ['It is certain.', 'It is decidedly so.',
-                     'Without a doubt.', 'Yes - definitely.', 
-                     'You may rely on it.', 'As I see it, yes.',
-                     'Most likely.', 'Outlook good.', 
-                     'Yes.', 'Signs point to yes.', 
-                     'Reply hazy, try again.', 'Ask again later.', 
-                     'Better not tell you now.', 'Cannot predict now.', 
-                     'Concentrate and ask again.', 'Do not count on it.', 
-                     'My reply is no.', 'My sources say no.', 
-                     'Outlook not so good.', 'Very doubtful.']
+        responses = ['It is certain.', 
+                     'It is decidedly so.',
+                     'Without a doubt.', 
+                     'Yes - definitely.', 
+                     'You may rely on it.', 
+                     'As I see it, yes.',
+                     'Most likely.', 
+                     'Outlook good.', 
+                     'Yes.', 
+                     'Signs point to yes.', 
+                     'Reply hazy, try again.', 
+                     'Ask again later.', 
+                     'Better not tell you now.', 
+                     'Cannot predict now.', 
+                     'Concentrate and ask again.', 
+                     'Do not count on it.', 
+                     'My reply is no.', 
+                     'My sources say no.', 
+                     'Outlook not so good.', 
+                     'Very doubtful.']
         
         return random.choice(responses)
 
