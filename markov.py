5a06618c18a5e55894413c1b7e599ab24b1f9a03
jwl-7 <lucidrainbows@gmail.com>
2019-04-29 13:25:31 -0400

    cleaned up redundant code and added docstrings

--- markov.py
+++ markov.py
@@ -1,60 +1,67 @@
-from dbhelper import DBHelper
 import random
 import string
 import re
+from dbhelper import Database
 
 BOT_NAME = '---> BOT NAME GOES HERE <---'
 
 class Markov:
+    """This class provides a simple Markov chain implementation to help
+    the bot generate funny sentences based on what other users
+    have already said in the Discord server.
     """
-    This class generates the Markov chain messages for the chatbot.
 
-    """
-
-    def __init__(self):
-        return
-
-    def fix_message(self, item_text):
-        """ reformats message to be used in Markov chain """
-
-        message = item_text
-        message = re.sub(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', message)
+    def fix_message(self, item):
+        """Reformats message to be used in the chain.
+        
+        Args:
+            item (str): Text that is grabbed from the sqlite db.
+            
+        Returns:
+            message (str): Text that has been stripped of all punctuation.
+        """
+        message = re.sub(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', item)
         de_punctuate = str.maketrans('', '', string.punctuation)
         message = message.translate(de_punctuate)
         message = message.lower()
         return message
 
-    def format_sentence(self, item_text):
-        """ adds some formatting to the generated sentence """
+    def format_sentence(self, unformatted_sentence):
+        """Adds formatting to generated sentence.
+        
+        Args:
+            unformatted_sentence (str): Unformatted generated sentence.
 
-        sentence = item_text
+        Returns:
+            formatted_sentence (str): Formatted generated sentence with first word capitalized and punctation at end.
+        """
         punctuation_list = ['!', '?', '.']
         chance = [0.2, 0.1, 0.9]
-
-        # capitalize first word and add punctuation to end of sentence
-        sentence = sentence.capitalize()
+        formatted_sentence = unformatted_sentence.capitalize()
         punctuation = random.choices(punctuation_list, chance)
         punctuation = str(punctuation[0])
-        sentence = sentence.rstrip()
-        sentence += punctuation
-        return sentence
+        formatted_sentence = formatted_sentence.rstrip()
+        formatted_sentence += punctuation
+        return formatted_sentence
 
     def create_chain(self):
-        """ create Markov chain from messages stored in the database """
-
+        """Creates Markov chain from messages stored in the sqlite db and generates sentence.
+        
+        Returns:
+            markov_sentence (str): Markov chain generated sentence.
+        """
         start_words = []
         word_dict = {}
         flag = 1
         count = 0
-        db = DBHelper()
+        db = Database()
         messages = db.get_items()
 
         for item in messages:
-
             # reformat messages and split the words into lists
             item = self.fix_message(item)
             temp_list = item.split()
-            
+
             # add the first word of each message to a list
             if (
                 len(temp_list) > 0 and 
@@ -65,7 +72,6 @@ class Markov:
 
             # create a dictionary of words that will be used to form the sentence
             for index, item in enumerate(temp_list):
-                
                 # add new word to dictionary
                 if temp_list[index] not in word_dict:
                     word_dict[temp_list[index]] = []
@@ -84,7 +90,6 @@ class Markov:
 
         # loop through the chain
         while flag == 1 and count < 100:
-
             # add word to sentence
             count += 1
             sentence += curr_word + ' '
@@ -98,5 +103,5 @@ class Markov:
                 flag = 0
         
         # format final sentence
-        sentence = self.format_sentence(sentence)
-        return sentence
+        markov_sentence = self.format_sentence(sentence)
+        return markov_sentence
