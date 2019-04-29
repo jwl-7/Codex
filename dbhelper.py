d51a2186f0fe6a0b3e60c0ddd90f6911e7c74aa3
jwl-7 <lucidrainbows@gmail.com>
2019-04-29 12:09:26 -0400

    code cleanup

--- dbhelper.py
+++ dbhelper.py
@@ -1,43 +1,34 @@
 import sqlite3
 
-class DBHelper:
-    """
-    This class provides helper functions to communicate 
-    with the sqlite database.
-
-    """
+class Database:
+    """This class provides helper functions to communicate with the sqlite database."""
 
     def __init__(self, dbname = 'chatlog.sqlite'):
-        """creates database connection"""
-
+        """Creates database connection."""
         self.dbname = dbname
         self.conn = sqlite3.connect(dbname)
 
     def setup(self):
-        """ creates new table and column """
-
+        """Creates new table and column."""
         stmt = 'CREATE TABLE IF NOT EXISTS msglog (messages text)'
         self.conn.execute(stmt)
         self.conn.commit()
 
     def add_item(self, item_text):
-        """ inserts item into table """
-
+        """Inserts item into table."""
         stmt = 'INSERT INTO msglog (messages) VALUES (?)'
         args = (item_text, )
         self.conn.execute(stmt, args)
         self.conn.commit()
     
     def delete_item(self, item_text):
-        """ deletes item from table """
-
+        """Deletes item from table."""
         stmt = 'DELETE FROM msglog WHERE messages = (?)'
         args = (item_text, )
         self.conn.execute(stmt, args)
         self.conn.commit()
 
     def get_items(self):
-        """ gets all items from table """
-        
+        """Gets all items from table."""
         stmt = 'SELECT messages FROM msglog'
         return [x[0] for x in self.conn.execute(stmt)]
