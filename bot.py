c3cf9245e83446ac795b798ae951a2b37ef63ef9
jwl-7 <lucidrainbows@gmail.com>
2019-04-29 14:00:59 -0400

    fixed commands

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ API_TOKEN = '---> DISCORD API TOKEN GOES HERE <---'
 BOT_NAME  = '---> BOT NAME GOES HERE <---'
 
 bot = discord.Client()
-db = DBHelper()
+db = Database()
 db.setup()
 
 @bot.event
@@ -21,36 +21,35 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     cmd = Commands()
-
     if message.author == bot.user:
         return
     elif message.content.startswith('!8ball'):
         fortune = cmd.magic_8ball()
         await message.channel.send(fortune + ' ' + message.author.mention)
+    elif message.content.startswith('!coin'):
+        coin_toss = cmd.coin_flip()
+        await message.channel.send(coin_toss + ' ' + message.author.mention)
     elif message.content.startswith('!help'):
         bot_help = cmd.help()
         await message.author.send(bot_help)
     elif message.content.startswith('!slots'):
         slot_spin = cmd.slot_machine()
         await message.channel.send(slot_spin + ' ' + message.author.mention)
-    elif message.content.startswith('!coin'):
-        coin_face = cmd.coin_flip()
-        await message.channel.send(coin_face + ' ' + message.author.mention)
     elif message.content.startswith('!sponge'):
         sponge_text = cmd.sponge(message.content)
         await message.channel.send(sponge_text)
     elif message.content.startswith('!cbs')
-        corporate_statement = cbs.generate_statement()
+        corporate_statement = cmd.corporate_bullshit()
         await message.channel.send(corporate_statement)
     elif message.content.startswith('!wisdom'):
-        wisdom_quote = wisdom.generate_quote()
-        await message.channel.send(wisdom_quote)
+        chopra_quote = cmd.wisdom()
+        await message.channel.send(chopra_quote)
     elif message.content.startswith('!') or message.attachments:
         return
     elif BOT_NAME in message.content.lower() or bot.user.mentioned_in(message):
         db.add_item(message.content)
         markov = Markov()
-        response = markov.create_chain()
+        response = markov.create_chain(BOT_NAME)
         await message.channel.send(response)
     else:
         db.add_item(message.content)
