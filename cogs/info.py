4184b30db0f36eea55d6a0a5cda0ad88f1a9087d
jwl-7 <lucidrainbows@gmail.com>
2020-01-19 15:37:19 -0500

    Change command list message

--- cogs/info.py
+++ cogs/info.py
@@ -26,15 +26,18 @@ class Info(commands.Cog):
     async def help(self, ctx):
         """!help - Bot Command List"""
         command_list = (
-            '```!8ball - magic 8-ball\n'
-            '!coin - heads or tails\n'
-            '!slots - slot machine\n'
-            '!sponge - spongebob meme text\n'
-            '!wisdom - deepak chopra quote\n'
-            '!cbs - corporate bullshit statement```'
+            '**!8ball** - magic 8-ball\n'
+            '**!coin** - heads or tails\n'
+            '**!slots** - slot machine\n'
+            '**!sponge** *<message>* - convert message to spongemock text\n'
+            '**!wisdom** - generate fake deepak chopra quote\n'
+            '**!cbs** - generate corporate bullshit'
             )
+        embed = discord.Embed(colour=discord.Colour.purple())
+        embed.set_author(name='Codex BOT - Command List')
+        embed.add_field(name=command_list, value='\u200b')
         try:
-            await ctx.author.send(command_list)
+            await ctx.author.send(embed=embed)
         except discord.Forbidden:
             print(f'[ERROR] Failed to send !help list to {ctx.author.name}')
 
