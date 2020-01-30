2c6515c676a6315e225d03d0fe63af88c47c0c7d
jwl-7 <lucidrainbows@gmail.com>
2020-01-30 02:12:29 -0500

    Move !8ball function

--- cogs/fun.py
+++ cogs/fun.py
@@ -16,6 +16,38 @@ class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command(name='8ball')
+    async def _8ball(self, ctx):
+        """!8ball - Magic 8-Ball"""
+        responses = [
+            'It is certain.',
+            'It is decidedly so.',
+            'Without a doubt.',
+            'Yes - definitely.',
+            'You may rely on it.',
+            'As I see it, yes.',
+            'Most likely.',
+            'Outlook good.',
+            'Yes.',
+            'Signs point to yes.',
+            'Reply hazy, try again.',
+            'Ask again later.',
+            'Better not tell you now.',
+            'Cannot predict now.',
+            'Concentrate and ask again.',
+            'Do not count on it.',
+            'My reply is no.',
+            'My sources say no.',
+            'Outlook not so good.',
+            'Very doubtful.'
+            ]
+        fortune = random.choice(responses)
+
+        embed = discord.Embed(colour=discord.Colour.purple())
+        embed.set_author(name='Magic 8-Ball', icon_url='https://i.imgur.com/XhNqADi.png')
+        embed.add_field(name=f'*{ctx.author.name}, your fortune says...*', value=f'**{fortune}**')
+        await ctx.send(embed=embed)
+
     @commands.command()
     async def coin(self, ctx):
         """!coin - Flip a coin."""
