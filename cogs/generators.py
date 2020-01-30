5bedea32f3f1903ac774161b28b3ef802d96fcfa
jwl-7 <lucidrainbows@gmail.com>
2020-01-29 23:19:19 -0500

    Add new jargon generators

--- cogs/generators.py
+++ cogs/generators.py
@@ -6,10 +6,12 @@ This module includes jargon generators.
 
 import random
 import discord
+import jargon.audio as audio
 import jargon.chopra as chopra
 import jargon.corporate as corp
 import jargon.education as edu
-
+import jargon.excuses as excuses
+import jargon.technology as tech
 from discord.ext import commands
 
 
@@ -18,21 +20,27 @@ class Generators(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def wisdom(self, ctx):
-        """!wisdom - Generates faux Deepak Chopra quote using randomly selected words."""
-        part1 = random.choice(chopra.words1)
-        part2 = random.choice(chopra.words2)
-        part3 = random.choice(chopra.words3)
-        part4 = random.choice(chopra.words4)
-        quote = f'{part1} {part2} {part3} {part4}.'
+    async def audiophile(self, ctx):
+        """!audiophile - Generates hipster audio jargon."""
+        words = {
+            0: random.choice(audio.words0),
+            1: random.choice(audio.words1),
+            2: random.choice(audio.words2),
+            3: random.choice(audio.words3),
+            4: random.choice(audio.words4)
+        }
+        sentence = random.choice(audio.sentences)
+        for x in range(5):
+            s = f'{{{x}}}'
+            sentence = sentence.replace(s, words[x])
 
         embed = discord.Embed(colour=discord.Colour.blue())
-        embed.add_field(name=f'Chopra Wisdom', value=f'**{quote}**')
+        embed.add_field(name=f'Audio Jargon', value=f'**{sentence}**')
         await ctx.send(embed=embed)
 
     @commands.command()
     async def corporate(self, ctx):
-        """!corporate - Generates corporate bulls**t statement using randomly selected words."""
+        """!corporate - Generates corporate bulls**t statement."""
         adverb = random.choice(corp.adverbs)
         verb = random.choice(corp.verbs)
         adjective = random.choice(corp.adjectives)
@@ -45,16 +53,68 @@ class Generators(commands.Cog):
 
     @commands.command()
     async def education(self, ctx):
-        """!education - Generates educational nonsense using randomly selected words."""
+        """!education - Generates educational nonsense."""
         verb = random.choice(edu.verbs)
         adjective = random.choice(edu.adjectives)
         noun = random.choice(edu.nouns)
-        phrase = random.choice(edu.phrase)
+        phrase = random.choice(edu.phrases)
         statement = f'We will {verb} {adjective} {noun} {phrase}.'
 
         embed = discord.Embed(colour=discord.Colour.blue())
         embed.add_field(name=f'Educational Nonsense', value=f'**{statement}**')
         await ctx.send(embed=embed)
+
+    @commands.command()
+    async def excuse(self, ctx):
+        """!excuse - Generates the perfect excuse."""
+        words = {
+            0: random.choice(excuses.words0),
+            1: random.choice(excuses.words1),
+            2: random.choice(excuses.words2),
+            3: random.choice(excuses.words3),
+            4: random.choice(excuses.words4)
+        }
+        sentence = random.choice(excuses.sentences)
+        for x in range(5):
+            s = f'{{{x}}}'
+            sentence = sentence.replace(s, words[x])
+
+        embed = discord.Embed(colour=discord.Colour.blue())
+        embed.add_field(name=f'Perfect Excuse', value=f'**{sentence}**')
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def technology(self, ctx):
+        """!technology - Generates hollywood tech jargon."""
+        words = {
+            0: random.choice(tech.words0),
+            1: random.choice(tech.words1),
+            2: random.choice(tech.words2),
+            3: random.choice(tech.words3),
+            4: random.choice(tech.words4)
+        }
+        sentence = random.choice(tech.sentences)
+        for x in range(5):
+            s = f'{{{x}}}'
+            sentence = sentence.replace(s, words[x])
+
+        embed = discord.Embed(colour=discord.Colour.blue())
+        embed.add_field(name=f'Tech Jargon', value=f'**{sentence}**')
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def wisdom(self, ctx):
+        """!wisdom - Generates faux Deepak Chopra quote."""
+        part0 = random.choice(chopra.words0)
+        part1 = random.choice(chopra.words1)
+        part2 = random.choice(chopra.words2)
+        part3 = random.choice(chopra.words3)
+        quote = f'{part0} {part1} {part2} {part3}.'
+
+        embed = discord.Embed(colour=discord.Colour.blue())
+        embed.add_field(name=f'Chopra Wisdom', value=f'**{quote}**')
+        await ctx.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(Generators(bot))
