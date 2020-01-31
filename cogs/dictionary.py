303af1583c606cd652307b39e6a1dcafba3fd626
jwl-7 <lucidrainbows@gmail.com>
2020-01-30 21:41:15 -0500

    Add !define

--- cogs/dictionary.py
+++ cogs/dictionary.py
@@ -8,12 +8,47 @@ import urllib.parse
 
 import requests
 import discord
+import yaml
 from discord.ext import commands
+
+
+with open('config.yml') as file:
+    config = yaml.safe_load(file)
 
 
 class Dictionary(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command()
+    async def define(self, ctx, *, search):
+        """!define - Search Merriam-Webster Dictionary."""
+        url = 'https://dictionaryapi.com/api/v3/references/collegiate/json/'
+        token = config['dictionary']['token']
+
+        r = requests.get(f'{url}{search}?key={token}')
+        try:
+            r.raise_for_status()
+        except requests.exceptions.HTTPError as error:
+            await ctx.send('Sorry, your **!define** search failed')
+            return print(f'[ERROR] {error}')
+
+        data = r.json()
+        word = data[0]['meta']['id']
+        pronunciation = data[0]['hwi']['prs'][0]['mw']
+        word_type = data[0]['fl']
+        definition = data[0]['shortdef'][0]
+        link = f'https://www.merriam-webster.com/dictionary/{search}'
+
+        embed = discord.Embed(colour=discord.Colour.blue())
+        embed.set_author(
+            name='Merriam-Webster Dictionary',
+            icon_url='https://i.imgur.com/rPvCVSQ.png'
+        )
+        embed.add_field(name=f'**{word}**', value=f'/{pronunciation}/', inline=False)
+        embed.add_field(name=f'**{word_type}**', value=f'{definition}', inline=False)
+        embed.add_field(name='**Search Result**', value=f'{link}', inline=False)
+        await ctx.send(embed=embed)
 
     @commands.command()
     async def udefine(self, ctx, *, search):
@@ -34,7 +69,10 @@ class Dictionary(commands.Cog):
         link = data['list'][0]['permalink']
 
         embed = discord.Embed(colour=discord.Colour.blue())
-        embed.set_author(name='Urban Dictionary')
+        embed.set_author(
+            name='Urban Dictionary',
+            icon_url='https://i.imgur.com/Ojxf6W5.png'
+        )
         embed.add_field(name=f'**{word}**', value=f'{definition}', inline=False)
         embed.add_field(name=f'**Example**', value=f'{example}', inline=False)
         embed.add_field(name='**Search Result**', value=f'{link}', inline=False)
