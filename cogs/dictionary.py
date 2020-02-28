4d93d554e1df44043bcafbce020c5bddb26c03c4
jwl-7 <lucidrainbows@gmail.com>
2020-02-27 23:25:36 -0500

    Fix error handling + minor optimizations

--- cogs/dictionary.py
+++ cogs/dictionary.py
@@ -4,11 +4,10 @@ This module includes dictionary commands.
 """
 
 
-import urllib.parse
-
-import requests
 import discord
+import requests
 import yaml
+
 from discord.ext import commands
 
 
@@ -24,60 +23,62 @@ class Dictionary(commands.Cog):
     async def define(self, ctx, *, search):
         """!define <search> - Search Merriam-Webster Dictionary."""
         url = 'https://dictionaryapi.com/api/v3/references/collegiate/json/'
+        web_url = 'https://www.merriam-webster.com/dictionary/'
+        icon_url = 'https://i.imgur.com/rPvCVSQ.png'
         token = config['dictionary']['token']
+        link = f'{web_url}{requests.utils.quote(search)}'
+        embed = discord.Embed(colour=discord.Colour.blue())
+        embed.set_author(name='Merriam-Webster Dictionary', icon_url=icon_url)
 
-        r = requests.get(f'{url}{search}?key={token}')
         try:
+            r = requests.get(f'{url}{search}?key={token}')
             r.raise_for_status()
-        except requests.exceptions.HTTPError as error:
-            await ctx.send('Sorry, your **!define** search failed')
-            return print(f'[ERROR] {error}')
+        except requests.exceptions.RequestException:
+            embed.add_field(name='Error', value='Failed to connect to the dictionary API')
+            return await ctx.send(embed=embed)
 
         data = r.json()
+        if not data or type(data[0]) is not dict:
+            embed.add_field(name='Search Failed', value=f'No definition found for *{search}*')
+            return await ctx.send(embed=embed)
+
         word = data[0]['meta']['id']
         pronunciation = data[0]['hwi']['prs'][0]['mw']
         word_type = data[0]['fl']
         definition = data[0]['shortdef'][0]
-        link = f'https://www.merriam-webster.com/dictionary/{search}'
-
-        embed = discord.Embed(colour=discord.Colour.blue())
-        embed.set_author(
-            name='Merriam-Webster Dictionary',
-            icon_url='https://i.imgur.com/rPvCVSQ.png'
-        )
         embed.add_field(name=f'**{word}**', value=f'/{pronunciation}/', inline=False)
-        embed.add_field(name=f'**{word_type}**', value=f'{definition}', inline=False)
-        embed.add_field(name='**Search Result**', value=f'{link}', inline=False)
+        embed.add_field(name=f'**{word_type}**', value=definition, inline=False)
+        embed.add_field(name='**Search Result**', value=link, inline=False)
         await ctx.send(embed=embed)
 
     @commands.command()
     async def udefine(self, ctx, *, search):
         """!udefine <search> - Search Urban Dictionary."""
         url = 'http://api.urbandictionary.com/v0/define?term='
+        icon_url = 'https://i.imgur.com/Ojxf6W5.png'
+        embed = discord.Embed(colour=discord.Colour.blue())
+        embed.set_author(name='Urban Dictionary', icon_url=icon_url)
 
-        r = requests.get(f'{url}{urllib.parse.quote_plus(search)}')
         try:
+            r = requests.get(f'{url}{search}')
             r.raise_for_status()
-        except requests.exceptions.HTTPError as error:
-            await ctx.send('Sorry, your **!udefine** search failed')
-            return print(f'[ERROR] {error}')
+        except requests.exceptions.RequestException:
+            embed.add_field(name='Error', value='Failed to connect to the dictionary API')
+            return await ctx.send(embed=embed)
 
         data = r.json()
+        if not data or not data['list']:
+            embed.add_field(name='Search Failed', value=f'No definition found for *{search}*')
+            return await ctx.send(embed=embed)
+
         word = data['list'][0]['word']
         definition = data['list'][0]['definition']
         example = data['list'][0]['example']
         link = data['list'][0]['permalink']
-
-        embed = discord.Embed(colour=discord.Colour.blue())
-        embed.set_author(
-            name='Urban Dictionary',
-            icon_url='https://i.imgur.com/Ojxf6W5.png'
-        )
-        embed.add_field(name=f'**{word}**', value=f'{definition}', inline=False)
-        embed.add_field(name=f'**Example**', value=f'{example}', inline=False)
-        embed.add_field(name='**Search Result**', value=f'{link}', inline=False)
+        embed.add_field(name=f'**{word}**', value=definition, inline=False)
+        embed.add_field(name=f'**Example**', value=example, inline=False)
+        embed.add_field(name='**Search Result**', value=link, inline=False)
         await ctx.send(embed=embed)
-
 
 def setup(bot):
     bot.add_cog(Dictionary(bot))
