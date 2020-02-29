ff582d1acd12bb4b5e5c40e898d8bcd609c1481f
jwl-7 <lucidrainbows@gmail.com>
2020-02-29 10:58:30 -0500

    Add !dog, !labrador, !pyrenees

--- cogs/dogs.py
+++ cogs/dogs.py
@@ -15,6 +15,66 @@ class Dogs(commands.Cog):
         self.bot = bot
 
     @commands.command()
+    async def dog(self, ctx):
+        """!dog - Display random dog image."""
+        url = 'https://dog.ceo/api/breeds/images/random'
+        embed = discord.Embed(colour=discord.Colour.blue())
+
+        try:
+            r = requests.get(url)
+            r.raise_for_status()
+        except requests.exceptions.RequestException:
+            embed = discord.Embed(
+                colour=discord.Colour.darker_grey(),
+                description='Failed to connect to the *Dog API*'
+            )
+            return await ctx.send(embed=embed)
+
+        image = r.json()['message']
+        embed.set_image(url=image)
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def labrador(self, ctx):
+        """!labrador - Display random Labrador image."""
+        url = 'https://dog.ceo/api/breed/labrador/images/random'
+        embed = discord.Embed(colour=discord.Colour.blue())
+
+        try:
+            r = requests.get(url)
+            r.raise_for_status()
+        except requests.exceptions.RequestException:
+            embed = discord.Embed(
+                colour=discord.Colour.darker_grey(),
+                description='Failed to connect to the *Dog API*'
+            )
+            return await ctx.send(embed=embed)
+
+        image = r.json()['message']
+        embed.set_image(url=image)
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def pyrenees(self, ctx):
+        """!pyrenees - Display random Pyrenees image."""
+        url = 'https://dog.ceo/api/breed/pyrenees/images/random'
+        embed = discord.Embed(colour=discord.Colour.blue())
+
+        try:
+            r = requests.get(url)
+            r.raise_for_status()
+        except requests.exceptions.RequestException:
+            embed = discord.Embed(
+                colour=discord.Colour.darker_grey(),
+                description='Failed to connect to the *Dog API*'
+            )
+            return await ctx.send(embed=embed)
+
+        image = r.json()['message']
+        embed.set_image(url=image)
+        await ctx.send(embed=embed)
+
+    @commands.command()
     async def shiba(self, ctx):
         """!shiba - Display random Shiba image."""
         url = 'https://dog.ceo/api/breed/shiba/images/random'
