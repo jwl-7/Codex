c1c5a5edb9797f716c8ef82a59e5eda431b91e66
jwl-7 <lucidrainbows@gmail.com>
2022-04-01 00:52:57 -0400

    Add animated slots

--- cogs/slots.py
+++ cogs/slots.py
@@ -18,14 +18,9 @@ class Slots(commands.Cog):
     async def slots(self, ctx):
         """.slots - Play fruit emojis slot machine."""
         icon_url = 'https://i.imgur.com/8oGuoyq.png'
-        slots = ['apple', 'watermelon', 'taco', 'cherries', 'doughnut', 'grapes']
-        slot1 = slots[random.randint(0, 5)]
-        slot2 = slots[random.randint(0, 5)]
-        slot3 = slots[random.randint(0, 5)]
-        slot4 = slots[random.randint(0, 5)]
-
-        slot_spin = f'|\t:{slot1}:\t|\t:{slot2}:\t|\t:{slot3}:\t|\t:{slot4}:\t|'
+        emojis = ['apple', 'cherries', 'doughnut', 'grapes', 'taco', 'watermelon']
         jackpot = '$$$ !!! JACKPOT !!! $$$'
+        jackshit = '(◕‿◕)╭∩╮ YOU GET NOTHING !!!'
 
         embed = disnake.Embed(colour=disnake.Colour.gold())
         embed.set_author(name='Slot Machine', icon_url=icon_url)
@@ -34,17 +29,34 @@ class Slots(commands.Cog):
             value='\u200b',
             inline=False
         )
+        slots = ['\t' for _ in range(4)]
+        slot_spin = self.slot_spin(slots)
+        embed.add_field(name=slot_spin, value='\u200b')
+        embed_dict = embed.to_dict()
+        slot_message = await ctx.send(embed=embed)
+
+        for slot_num in range(4):
+            for _ in range(5):
+                slot = random.choice(emojis)
+                slot = f':{slot}:'
+                slots[slot_num] = slot
+                slot_spin = self.slot_spin(slots)
+                embed_dict['fields'][1]['name'] = slot_spin
+                embed = disnake.Embed.from_dict(embed_dict)
+                await slot_message.edit(embed=embed)
 
         if (
-            slot1 == slot2 and slot3 == slot4 or
-            slot1 == slot3 and slot2 == slot4 or
-            slot1 == slot4 and slot2 == slot3
+            slots[0] == slots[1] and slots[2] == slots[3] or
+            slots[0] == slots[2] and slots[1] == slots[3] or
+            slots[0] == slots[3] and slots[1] == slots[2]
         ):
-            embed.add_field(name=slot_spin, value='\u200b')
             embed.set_footer(text=jackpot)
         else:
-            embed.add_field(name=slot_spin, value='\u200b')
-        await ctx.send(embed=embed)
+            embed.set_footer(text=jackshit)
+        await slot_message.edit(embed=embed)
+
+    def slot_spin(self, slots):
+        return f'|\t{slots[0]}\t|\t{slots[1]}\t|\t{slots[2]}\t|\t{slots[3]}\t|'
 
 
 def setup(bot):
